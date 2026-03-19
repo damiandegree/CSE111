f010dba5a7eb08dac38f9fb39e6df069a49ad451
damiandegree <damian.degree96@gmail.com>
2026-03-19 17:04:02 -0300

    .

--- W03-Code-Activities/test_words.py
+++ W03-Code-Activities/test_words.py
@@ -9,9 +9,6 @@ def test_prefix():
     Parameters: none
     Return: nothing
     """
-    # Call the prefix function and verify that it returns a string.
-    pre = prefix("upbeat", "upgrade")
-    assert isinstance(pre, str), "prefix function must return a string"
 
     # Call the prefix function ten times and use an assert
     # statement to verify that the string returned by the
@@ -26,6 +23,25 @@ def test_prefix():
     assert prefix("jump", "joyous") == "j"
     assert prefix("upbeat", "upgrade") == "up"
     assert prefix("Disable", "dIstasteful") == "dis"
+
+    #Call the suffix function and verify that it returns a string.
+def test_suffix():
+
+    #Now I have to test the function as the example below
+    #Call the suffix function
+
+    assert suffix("", "") == ""
+    assert suffix("", "correct") == ""
+    assert suffix("clear", "") == ""
+    assert suffix("angelic", "awesome") == ""
+    assert suffix("found", "profound") == "found"
+    assert suffix("ditch", "itch") == "itch"
+    assert suffix("happy", "funny") == "y"
+    assert suffix("tired", "fatigued") == "ed"
+    assert suffix("swimming", "FLYING") == "ing"
+
+
+    
 
 
 # Call the main function that is part of pytest so that the
