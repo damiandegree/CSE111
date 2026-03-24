c62f5fa6ef0d65ab4d257f0d14f5c976f53ddb82
damiandegree <damian.degree96@gmail.com>
2026-03-24 00:50:07 -0300

    .

--- W03-Code-Activities/water_flow.py
+++ W03-Code-Activities/water_flow.py
@@ -54,16 +54,16 @@ def pressure_loss_from_pipe(pipe_diameter, pipe_length, friction_factor, fluid_v
     return numerator / denominator
 
 def pressure_loss_from_fittings(fluid_velocity, quantity_fittings):
-    return -.04 * WATER_DENSITY * fluid_velocity * 2 * quantity_fittings / 2000
+    return (-0.04 * WATER_DENSITY * fluid_velocity ** 2 * quantity_fittings) / 2000
 
 def reynolds_number(hydraulic_diameter, fluid_velocity):
       #TODO: Need to implement
       p = WATER_DENSITY
-      hydraulic_diameter = PVC_SCHED80_INNER_DIAMETER
-      fluid_velocity = SUPPLY_VELOCITY
+      d =hydraulic_diameter
+      v = fluid_velocity
       u =DYNAMIC_VISCOSITY_WATER
 
-      return (p*hydraulic_diameter*fluid_velocity)/u
+      return (p*d*v)/u
 
 def pressure_loss_from_pipe_reduction(larger_diameter, fluid_velocity, reynolds_number, smaller_diameter):
     k=(.1 + 50 / reynolds_number) * ((larger_diameter / smaller_diameter) ** 4 - 1)
